659d1e343a10c5df8af17eac9db8b96f7b4634e4
root <parny_fan2003@doglover.com>
2024-10-01 22:28:05 +0100

    basic cooker

--- src/cooking/tasks/BankTask.java
+++ src/cooking/tasks/BankTask.java
@@ -1,5 +1,8 @@
 package cooking.tasks;
 
+import org.rspeer.game.adapter.component.inventory.Backpack;
+import org.rspeer.game.adapter.component.inventory.Bank;
+import org.rspeer.game.component.Inventories;
 import org.rspeer.game.script.Task;
 import org.rspeer.game.script.TaskDescriptor;
 
@@ -8,6 +11,19 @@ public class BankTask extends Task {
 
   @Override
   public boolean execute() {
-    return false;
+    Backpack inv = Inventories.backpack();
+    if (inv.contains(iq -> iq.names("Raw shrimps").results())) {
+      return false;
+    }
+
+    if (!Bank.isOpen()) {
+      Bank.open();
+      return true;
+    }
+
+    Bank bank = Inventories.bank();
+    bank.depositAllExcept(iq -> iq.names("Raw shrimps").results());
+    bank.withdrawAll(iq -> iq.names("Raw shrimps").results().first());
+    return true;
   }
 }
--- src/cooking/tasks/CookTask.java
+++ src/cooking/tasks/CookTask.java
@@ -1,5 +1,13 @@
 package cooking.tasks;
 
+import org.rspeer.commons.logging.Log;
+import org.rspeer.game.adapter.component.inventory.Backpack;
+import org.rspeer.game.adapter.scene.Player;
+import org.rspeer.game.adapter.scene.SceneObject;
+import org.rspeer.game.component.Inventories;
+import org.rspeer.game.component.Production;
+import org.rspeer.game.scene.Players;
+import org.rspeer.game.scene.SceneObjects;
 import org.rspeer.game.script.Task;
 import org.rspeer.game.script.TaskDescriptor;
 
@@ -8,6 +16,33 @@ public class CookTask extends Task {
 
   @Override
   public boolean execute() {
-    return false;
+    Backpack inv = Inventories.backpack();
+    if (!inv.contains(iq -> iq.names("Raw shrimps").results())) {
+      return false;
+    }
+
+    SceneObject fire = SceneObjects.query()
+        .ids(43475)
+        .results()
+        .nearest();
+    if (fire == null) {
+      Log.warn("Make sure you start the script in Rogues den!");
+      return false;
+    }
+
+    Player self = Players.self();
+    if (self.isAnimating()) {
+      sleepUntil(() -> !self.isAnimating(), () -> self.isAnimating(), 2);
+      return false;
+    }
+
+    Production production = Production.getActive();
+    if (production != null && production.isOpen()) {
+      production.initiate(0);
+    } else {
+      inv.use(iq -> iq.names("Raw shrimps").results().first(), fire);
+    }
+
+    return true;
   }
 }
